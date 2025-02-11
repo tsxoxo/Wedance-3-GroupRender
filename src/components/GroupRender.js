bf251a2ef1ad1d153ce01e651d37fe7461cb5dbb
Tom Szwaja <59713582+tsxoxo@users.noreply.github.com>
2025-02-11 18:06:23 +0000

    Write down useful snippets from docs; add function call to slots.default()

--- src/components/GroupRender.js
+++ src/components/GroupRender.js
@@ -1,7 +1,16 @@
-import { h } from 'vue';
+import { h } from "vue"
 
 export default {
-  setup(props, { slots }) {
-    return () => h('div', props, slots.default);
-  },
-};
+    setup(props, { slots }) {
+        // Goal: Render all the elements passed via slots with custom classes
+        //
+        // possibly use smth like this:
+        // Array.from({ length: 20 }).map(() => {
+        //   return h('p', 'hi')
+        // })
+        //
+        // Attributes:
+        // h('div', { class: 'bar', innerHTML: 'hello' })
+        return () => h("div", { class: "bg-red-500" }, props, slots.default())
+    },
+}
