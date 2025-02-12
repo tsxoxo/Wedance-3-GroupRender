a28be78526af190283f1811f81c20d80a12941d8
Tom Szwaja <59713582+tsxoxo@users.noreply.github.com>
2025-02-12 17:55:53 +0000

    Fix: add classes to each slots.default child

--- src/components/GroupRender.js
+++ src/components/GroupRender.js
@@ -17,8 +17,22 @@ export default {
         // How to add/replace classes on the slots.default...
         // ... a recursive h()?
         return () =>
-            h("div", { class: "border rounded-lg flex divide-x" }, props, [
-                slots.default({ class: "foo" }),
-            ])
+            h(
+                "div",
+                { class: "border rounded-lg flex divide-x" },
+                props,
+                slots.default().map(component => {
+                    console.log(component)
+
+                    console.log(component.data)
+
+                    component.data = {
+                        class: "foobar",
+                    }
+                    return h(component, {
+                        class: "px-4 py-2 hover:bg-gray-100",
+                    })
+                })
+            )
     },
 }
