2a761a6182d3e71bbec0c5e2f93cdbe95331dafd
Tom Szwaja <59713582+tsxoxo@users.noreply.github.com>
2025-02-13 16:30:01 +0000

    Add test for correct rendering and modification of class attribute

--- src/components/GroupRender.test.js
+++ src/components/GroupRender.test.js
@@ -1,7 +1,22 @@
 import { mount } from "@vue/test-utils"
 import { test } from "vitest"
-import App from "../App.vue"
+import GroupRender from "./GroupRender.js"
+import Tbutton from "./TButton.vue"
 
-test("foo", () => {
-    const wrapper = mount(App)
+test("Slot-children are being rendered and class attribute is being correctly modified", () => {
+    const wrapper = mount(GroupRender, {
+        slots: {
+            default: [Tbutton, Tbutton],
+        },
+    })
+    console.log(wrapper.findAll("button")[0].attributes())
+    // 2 <button> elements are being rendered
+    expect(wrapper.findAll("button").length).toBe(2)
+    // They have the classes specified in the criteria
+    expect(wrapper.findAll("button")[0].attributes().class).toBe(
+        "px-4 py-2 hover:bg-gray-100"
+    )
+    expect(wrapper.findAll("button")[0].attributes().class).toBe(
+        "px-4 py-2 hover:bg-gray-100"
+    )
 })
